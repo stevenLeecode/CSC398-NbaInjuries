7499f9082d8d743c3413d9e3ecbe6912c9dc71ee
SteveLee6177 <splee6177@gmail.com>
2024-11-20 02:49:28 -0500

    Adding tasks

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import pandas as pd
 injury_df = pd.read_csv('NBA Player Injury Stats(1951 - 2023).csv')
 players_df = pd.read_csv('Player Totals.csv')
 
+print(injury_df)
+
 #Issue, we need to merge on a column that is the same in both dataframes.
-df = injury_df.merge(players_df, how='outer')
-print(df)
+#df = injury_df.merge(players_df, how='outer')
+#print(df)
